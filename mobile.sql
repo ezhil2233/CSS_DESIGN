fa714b16b844cf267af97b0ecc5c2c626a6f2245
ezhil2233 <88583894+ezhil2233@users.noreply.github.com>
2025-11-22 19:38:44 +0530

    Add categories and suppliers table structure
    Define tables for categories and suppliers in mobile.sql

---
+++ mobile.sql
@@ -0,0 +1,21 @@
+categoery
+
+id
+categoery_name -- Head set, Mobile,Phone cover
+categoery_description -- about mobile, headset
+
+
+
+supplier
+
+id
+supplier_name
+contact_person
+email
+phone_number
+address
+
+when show stock details -> supplier,categoery and product
+
+
+
